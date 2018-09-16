e085dd5c984cdaac5fc9b6623a5900e1b2d16bb2
guizamberlam <guizamberlam@hotmail.com>
2018-09-16 01:52:30 -0300

    Equipamento Controller Adicionado

--- src/main/java/com/projeto/estacionai/controller/EquipamentoController.java
+++ src/main/java/com/projeto/estacionai/controller/EquipamentoController.java
@@ -1,5 +1,7 @@
 package com.projeto.estacionai.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.projeto.estacionai.model.Equipamento;
+import com.projeto.estacionai.repository.EquipamentoRepositorySearch;
 import com.projeto.estacionai.service.EquipamentoService;
 
 @Controller
@@ -23,18 +26,24 @@ public class EquipamentoController {
 	@Autowired
 	private EquipamentoService service;
 	
-	//@Autowired
-	//private EquipamentoRepositorySearch search;
+	@Autowired
+	private EquipamentoRepositorySearch search;
 	
 	@GetMapping
 	public ModelAndView listar(Equipamento filtro)
 	{
 		
 		ModelAndView mv = new ModelAndView("equipamentos/v-lista-equipamento");
-		//filtro.setAtivo(true);
-		//mv.addObject("equipamentos", search.filtrar(filtro));
-		//mv.addObject("filtro", filtro);
+		filtro.setAtivo(true);
+		mv.addObject("equipamentos", search.filtrar(filtro));
+		mv.addObject("filtro", filtro);
 		return mv;
+	}
+	
+	@PostMapping
+	public ModelAndView listarEspecifico(Equipamento filtro)
+	{
+		return listar(filtro);
 	}
 	
 	@GetMapping("/novo")
@@ -43,5 +52,50 @@ public class EquipamentoController {
 		ModelAndView mv = new ModelAndView("equipamentos/v-cadastro-equipamento");
 		mv.addObject(equipamento);			
 		return mv;
+	}
+	
+	@GetMapping("/editar/{id}")
+	public ModelAndView editar(@PathVariable Long id)
+	{
+		ModelAndView mv = new ModelAndView("equipamentos/v-editar-equipamento");
+		mv.addObject("equipamento", service.buscar(id));
+		return mv;
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deletar(@PathVariable Long id, RedirectAttributes attributes)
+	{
+		service.deletar(id);
+		
+		attributes.addFlashAttribute("mensagem", "Equipamento removido com sucesso!");
+		
+		return "redirect:/equipamentos";
+	}
+	
+	@PostMapping("/novo")
+	public ModelAndView salvar(@Valid Equipamento equipamento, BindingResult result, RedirectAttributes redirectAttributes)
+	{
+				
+		
+		if(result.hasErrors())
+		{
+			return novo(equipamento);
+		}
+		
+		
+		if(equipamento.getId() == null)
+		{
+			
+			this.service.salvar(equipamento);
+			redirectAttributes.addFlashAttribute("mensagem", "A conta foi cadastrada com sucesso!");
+			return new ModelAndView("redirect:/contas/pagar/novo");
+		}
+		else
+		{
+			this.service.salvar(equipamento);
+			redirectAttributes.addFlashAttribute("mensagem", "A conta foi atualizada com sucesso!");
+			return new ModelAndView("redirect:/contas/pagar/editar/" + equipamento.getId());
+		}
+		
 	}
 }
