2f070a046a7017491e9eb45b1d195f02db1e49e4
guizamberlam <guizamberlam@hotmail.com>
2018-10-31 18:31:17 -0300

    Criado Mensalidade Repository Search

---
+++ src/main/java/com/projeto/estacionai/repository/MensalidadeRepositorySearch.java
@@ -0,0 +1,50 @@
+package com.projeto.estacionai.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.stereotype.Repository;
+
+import com.projeto.estacionai.model.Mensalidade;
+
+@Repository
+public class MensalidadeRepositorySearch {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	
+	public List<Mensalidade> filtrar(Mensalidade mensalidade) {
+		CriteriaBuilder builder =  manager.getCriteriaBuilder();
+		CriteriaQuery<Mensalidade> criteria = builder.createQuery(Mensalidade.class);
+		
+		Root<Mensalidade> root = criteria.from(Mensalidade.class);
+		
+		Predicate[] predicates = restricoes(mensalidade, builder, root);
+		criteria.where(predicates);
+		
+		TypedQuery<Mensalidade> query = manager.createQuery(criteria);
+		
+		return query.getResultList(); 
+	}
+	
+	private Predicate[] restricoes(Mensalidade mensalidade, CriteriaBuilder builder, Root<Mensalidade> root) {
+		List<Predicate> predicates	= new ArrayList<>();
+		
+	
+		
+		
+		
+		return predicates.toArray(new Predicate[predicates.size()]);
+	}
+	
+	
+}
